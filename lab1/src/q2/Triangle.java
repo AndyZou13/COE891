8ccad1dc6f95c381bcf6c58f3611ebe272b771db
Andy Zou <andyzou1302@gmail.com>
2024-01-19 13:57:14 -0500

    update lab1

--- lab1/src/q2/Triangle.java
+++ lab1/src/q2/Triangle.java
@@ -7,10 +7,13 @@ public class Triangle {
 		this.side2 = side2;
 		this.side3 = side3;
 	}
-	public double calculateArea () {
+	public double calculateArea () throws Exception {
+		if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+			throw new Exception("side length less than 0");
+		}
 		double s = (side1 + side2 + side3) * 0.5;
 		System.out.println("\t s=" + s);
-		double result = Math.sqrt(s * (side1 - s) * (side2 - s) * (side3 - s));
+		double result = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
 		System.out.println("\t result = " + result);
 		return result;
 	}
